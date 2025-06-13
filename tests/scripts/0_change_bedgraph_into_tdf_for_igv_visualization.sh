363be0b134e04a71a1579a96141ef247f721f7c8
Faye Valentine <fayevalentine@Mac.fritz.box>
2025-06-13 12:11:22 +0200

    tests: add/modify IGV‐TDF conversion helper script

---
+++ tests/scripts/0_change_bedgraph_into_tdf_for_igv_visualization.sh
@@ -0,0 +1,16 @@
+# conda create -n tools_igvtools
+# conda activate tools_igvtools
+# conda activate -c bioconda -c conda-forge igvtools=2.16.2
+genome="hg19"
+src_dir="../test_data_chr20"
+
+# genome="mm10"
+# src_dir="../test_ChIPseq_data_chr1_from_NatureGenetics2011"
+
+
+for bg in "$src_dir"/*.bedgraph; do
+  base="${bg%.bedgraph}"
+  echo ${base}
+  tdf=${base}.tdf
+
+  igvtools toTDF ${bg} ${tdf} ${genome}
